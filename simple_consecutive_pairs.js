47ddd271eee0af167dd8d9b20cbf09bd2ece24c9
Alice Ruppert <aliceruppert@gmail.com>
2020-09-25 00:54:59 -0600

    Add challenge to search and count consecutive number pairs in an array

---
+++ simple_consecutive_pairs.js
@@ -0,0 +1,38 @@
+/*
+In this Kata your task will be to return the count of pairs that have consecutive numbers as follows:
+
+pairs([1,2,5,8,-4,-3,7,6,5]) = 3
+The pairs are selected as follows [(1,2),(5,8),(-4,-3),(7,6),5]
+--the first pair is (1,2) and the numbers in the pair are consecutive; Count = 1
+--the second pair is (5,8) and are not consecutive
+--the third pair is (-4,-3), consecutive. Count = 2
+--the fourth pair is (7,6), also consecutive. Count = 3.
+--the last digit has no pair, so we ignore.
+*/
+
+//my Solution
+
+function pairs(ar){
+  let count = 0;
+  for (var i = 0; i < ar.length-1; i+=2) {
+    if ((ar[i] - ar[i+1]) === 1 || (ar[i] - ar[i+1]) === -1) {
+      count += 1;
+    };
+  };
+  return count;
+};
+
+//other solutions
+
+function pairs(array, count = 0) {
+  for (let i = 0; i < array.length; i += 2)
+    if (Math.abs(array[i] - array[i + 1]) === 1)
+      count += 1;
+  return count;
+}
+
+//or
+
+function pairs(ar){
+  return ar.length && (Math.abs(ar[0]-ar[1])==1) + pairs(ar.slice(2))
+}
